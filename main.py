7c33bdccc578f47251559fe37ff59ce4a8852025
Ian Sinnott <ian@iansinnott.com>
2019-10-04 09:59:05 +0800

    Add py script

---
+++ main.py
@@ -0,0 +1,33 @@
+import sqlite3
+import csv
+
+simplified = set()
+studied = set()
+
+# Grab all individual characters from my flashcard db. It's the full DB, so it
+# will have any individual characters stored there. Not just in my character
+# study category
+with sqlite3.connect('./tmp/pleco-flashcard-backup_191003.pqb') as db:
+    c = db.cursor()
+    for simp, trad in c.execute(
+            "SELECT hw,althw FROM pleco_flash_cards WHERE LENGTH(hw)=1"):
+        simplified.add(simp)
+
+# Grab all the chars from the 3000 list I found online
+with open('./tmp/3000-most-common.csv') as f:
+    reader = csv.reader(f)
+    for row in reader:
+        character = row[0]
+        simplified.add(character)
+
+# Grab all the chars that are currently in my character study list
+with open('./tmp/chars_2019-10-03.txt') as f:
+    for line in f:
+        studied.add(line[0])
+
+for character in simplified.difference(studied):
+    print(character)
+
+print('ALL: {}'.format(len(simplified)))
+print('STUDIED: {}'.format(len(studied)))
+print(len(simplified - studied))
