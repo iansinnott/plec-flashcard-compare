d5afc6979ad4c5938ccabe492719e8f03adfc9d5
Ian Sinnott <ian@iansinnott.com>
2019-10-04 10:03:52 +0800

    Refactor tmp to data

--- main.py
+++ main.py
@@ -7,21 +7,21 @@ studied = set()
 # Grab all individual characters from my flashcard db. It's the full DB, so it
 # will have any individual characters stored there. Not just in my character
 # study category
-with sqlite3.connect('./tmp/pleco-flashcard-backup_191003.pqb') as db:
+with sqlite3.connect('./data/pleco-flashcard-backup_191003.pqb') as db:
     c = db.cursor()
     for simp, trad in c.execute(
             "SELECT hw,althw FROM pleco_flash_cards WHERE LENGTH(hw)=1"):
         simplified.add(simp)
 
 # Grab all the chars from the 3000 list I found online
-with open('./tmp/3000-most-common.csv') as f:
+with open('./data/3000-most-common.csv') as f:
     reader = csv.reader(f)
     for row in reader:
         character = row[0]
         simplified.add(character)
 
 # Grab all the chars that are currently in my character study list
-with open('./tmp/chars_2019-10-03.txt') as f:
+with open('./data/chars_2019-10-03.txt') as f:
     for line in f:
         studied.add(line[0])
 
